65a79e91e014ed9072cf3b9f95d758b057af5fb3
ᴍ ʀ ⃞ ɪ ꜱ ᴜ ʀ ᴜ <156693573+MrIsuru@users.noreply.github.com>
2024-01-19 18:39:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,10 @@ const chalk = require('chalk');
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = {
       BASE_URL: "https://queen-api-production.up.railway.app", //Queen-AI Base API Url
-      SESSION_ID: process.env.SESSION_ID || "PASTE HERE", //enter your session id here
-      OWNER_NUMBER: process.env.OWNER_NUMBER || "9277XXXXXX", //Owner whatsapp number here
-      OWNER_NAME: process.env.OWNER_NAME || "DarkWinzo", //Queen-AI Owner Name here
+      SESSION_ID: process.env.SESSION_ID || "QUEEN-AI;;;BackyardExchange
+", //enter your session id here
+      OWNER_NUMBER: process.env.OWNER_NUMBER || "94766632281", //Owner whatsapp number here
+      OWNER_NAME: process.env.OWNER_NAME || "ᴍ ʀ ⃞ ɪ ꜱ ᴜ ʀ ᴜ", //Queen-AI Owner Name here
       PREFIX: ".", //Don't Change This..
       LANGUAGE: process.env.LANGUAGE || "SI" //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu /FR = French
 };
